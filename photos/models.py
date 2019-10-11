45901cee5d1656451119825759d0e78c633ebaec
niEmerance <niyemera6@gmail.com>
2019-10-11 15:57:20 +0300

    create class in models

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,25 @@
 from django.db import models
-
 # Create your models here.
+
+class ImageLocation(models.Model):
+    locName=models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.locName
+
+class ImageCategory(models.Model):
+    imgCategory=models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.imgCategory
+class Image(models.Model):
+    imgName=models.CharField(max_length =30)
+    imgDesc=models.TextField()
+    imgLoc=models.ForeignKey(ImageLocation)
+    imgCtgry=models.ForeignKey(ImageCategory)
+
+    def __str__(self):
+        return self.imgName
+
+    def save_editor(self):
+        self.save()
