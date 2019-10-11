fc65d0561e40c5f99782462ff86b7a91b4bd34f8
niEmerance <niyemera6@gmail.com>
2019-10-11 12:04:07 +0300

    Adding Bootstrap and Navigation Bars in My Project

---
+++ photos/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url,include
+from . import views
+
+urlpatterns=[
+    url('^$',views.welcome,name = 'welcome'),
+    url('^', views.gallery,name='gallery'),
+]
--- photos/views.py
+++ photos/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
-
+from django.http  import HttpResponse
+import datetime as dt
 
 # Create your views here.
+def welcome(request):
+    return render(request,'index.html')
+
+def gallery(request):
+    date = dt.date.today()
+    return render(request, 'all-picsPages/gallery.html', {"date": date,})
